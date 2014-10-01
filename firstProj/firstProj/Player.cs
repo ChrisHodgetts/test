e693c1468c69aa714b00ca7e9f8dcd7a0fb10a1a
unknown <h006532c@k106-11.student.staffs.ac.uk>
2014-10-01 11:30:29 +0100

    created an empty player class

---
+++ firstProj/firstProj/Player.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace firstProj
+{
+	public class Player
+	{
+		public string score;
+		public int name;
+		public string posx;
+		public string posy;
+		public Player ()
+		{
+		}
+		
+		public void getName()
+		{	
+		}
+		
+		public void setName()
+		{	
+		}
+		
+		public void getPosx()
+		{
+		}
+		
+		public void getPosy()
+		{	
+		}
+		
+		public void getScore()
+		{
+				
+		}
+	}
+}
+
